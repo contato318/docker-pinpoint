7ed333c63278e54d5eeed40fb3249be5a602d34b
Dirceu Silva <contato318@me.com>
2017-09-29 10:23:16 -0300

    Update start-web.sh

--- pinpoint-web/start-web.sh
+++ pinpoint-web/start-web.sh
@@ -12,6 +12,11 @@ HBASE_PORT=${HBASE_PORT:-2181}
 DISABLE_DEBUG=${DISABLE_DEBUG:-true}
 DISABLE_ANALYTICS=${DISABLE_ANALYTICS:-true}
 
+JDBC_DRIVER=${JDBC_DRIVER:-com.mysql.jdbc.Driver}
+JDBC_URL=${JDBC_URL:-jdbc:mysql://mysql:3306/pinpoint?characterEncoding=UTF-8}
+JDBC_USERNAME=${JDBC_USERNAME:-admin}
+JDBC_PASSWORD=${JDBC_PASSWORD:-admin}
+
 cp /assets/pinpoint-web.properties /usr/local/tomcat/webapps/ROOT/WEB-INF/classes/pinpoint-web.properties
 cp /assets/hbase.properties /usr/local/tomcat/webapps/ROOT/WEB-INF/classes/hbase.properties
 
@@ -21,6 +26,13 @@ sed -i "s/admin.password=admin/admin.password=${ADMIN_PASSWORD}/g" /usr/local/to
 
 sed -i "s/hbase.client.host=localhost/hbase.client.host=${HBASE_HOST}/g" /usr/local/tomcat/webapps/ROOT/WEB-INF/classes/hbase.properties
 sed -i "s/hbase.client.port=2181/hbase.client.port=${HBASE_PORT}/g" /usr/local/tomcat/webapps/ROOT/WEB-INF/classes/hbase.properties
+
+
+sed -i "s/jdbc.properties/ /g" /usr/local/tomcat/webapps/ROOT/WEB-INF/classes/jdbc.properties
+sed -i "s/jdbc.url=jdbc.*$/jdbc.url=${JDBC_URL}/g" /usr/local/tomcat/webapps/ROOT/WEB-INF/classes/jdbc.properties
+sed -i "s/jdbc.username=.*$/jdbc.username=${JDBC_USERNAME}/g" /usr/local/tomcat/webapps/ROOT/WEB-INF/classes/jdbc.propertie
+sed -i "s/jdbc.password=.*$/jdbc.password=${JDBC_PASSWORD}/g" /usr/local/tomcat/webapps/ROOT/WEB-INF/classes/jdbc.propertie
+
 
 if [ "$DISABLE_DEBUG" == "true" ]; then
     sed -i 's/level value="DEBUG"/level value="INFO"/' /usr/local/tomcat/webapps/ROOT/WEB-INF/classes/log4j.xml
